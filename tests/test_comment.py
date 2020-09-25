7072f46a8ab2916583afbf0c785b3ffec1e3c2e8
ruthjomo <ruthjomo19@gmail.com>
2020-09-25 09:44:16 +0300

    added more changes

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -2,24 +2,29 @@ from app.models import User,Comment,Pitch
 from app import db
 import unittest
 
-class TestComment(unittest.TestCase):
+class CommentModelTest(unittest.TestCase):
+
     def setUp(self):
-        self.pitch=Pitch(pitch='laugh now,cry later', pitch_category='sales pitch')
-        self.comment=Comment(comment='i like it',pitch=self.pitch)
+        self.user_Ruth = User(
+            username='Ruth', password='2603@ruth', email='ruthjomo19@gmail.com')
+        self.new_comment = Comment(
+            comment='laugh now,cry later', user=self.user_Ruth, pitch_id=12)
 
     def tearDown(self):
-        self.comment.query.delete()
-        self.pitch.query.delete()
+        Comment.query.delete()
+        User.query.delete()
 
-    def test_comment_instance(self):
-        self.assertEquals(self.comment.comment,'i like it')
-        self.assertEquals(self.comment.pitch,self.pitch)
+    def test_check_instance_variable(self):
+        self.assertEquals(self.new_comment.comment, Great)
+        self.assertEquals(self.new_comment.user, self.user_Ruth)
+        self.assertEquals(self.new_comment.pitch_id, 12)
 
-    def test_save(self):
-        self.comment.save_comment()
-        self.assertTrue(len(Comment.query.all())>0)
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        self.assertTrue(len(Comment.query.all()) > 0)
 
-    def test_get_comment(self):
-        self.comment.save_comment()
-        get=Comment.get_comments(self.pitch.id)
-        self.assertTrue(len(get)==1)
+    def test_get_comment_by_id(self):
+
+        self.new_comment.save_comment()
+        got_comments = Comment.get_comments(12)
+        self.assertTrue(len(got_comments) == 1)
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,18 +1,17 @@
 import unittest
 from app.models import User
 
-class TestUser(unittest.TestCase):
-    def setUp(self):
-        self.user=User(password='Access')
-    #tests if user password has been set to the password_hash
-    def test_password(self):
-        self.assertTrue(self.user.password_hash is not None)
+class UserModelTest(unittest.TestCase):
 
-    #tests if the password has been verfied
-    def test_password_verified(self):
-        self.assertTrue(self.user.verify_password('Access'))
-    
-    #tests if attribute error is raised
-    def test_attribute_error_raised(self):
-        with self.assertRaises(AttributeError):
-            self.user.password
+    def setUp(self):
+        self.new_user = User(password = 'Access')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_no_access_password(self):
+            with self.assertRaises(AttributeError):
+                self.new_user.password
+
+    def test_password_verification(self):
+            self.assertTrue(self.new_user.verify_password('Access'))
