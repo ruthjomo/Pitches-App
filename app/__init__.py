2cc4d4067f7d567ce5e25ec4903292e56110fdd9
ruthjomo <ruthjomo19@gmail.com>
2020-09-26 13:57:11 +0300

    added codes

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_uploads import UploadSet,configure_uploads,IMAGES,secure_filename
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
